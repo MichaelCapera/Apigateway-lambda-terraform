4d5bf703eaab10160bf43b10039920c527e4c9ef
MAC <michaelcapera@gmail.com>
2023-07-05 09:52:18 -0600

    Update payload

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_lambda_function" "test" {
 
 }
 
-############  API GATEWAY ###############
+#################  API GATEWAY ###################
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
