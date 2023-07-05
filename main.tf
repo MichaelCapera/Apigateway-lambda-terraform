3cd45e6ae68227c2e697c87882ca6ca0b9227f3b
MAC <michaelcapera@gmail.com>
2023-07-05 10:49:11 -0600

    Test deploy api with OAS

--- main.tf
+++ main.tf
@@ -1,124 +1,49 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+provider "aws" {
+  region = "us-east-1"  # Cambia esto a la región deseada
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "lambda_payload.zip"  # Reemplaza esto con la ruta a tu código Lambda
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "example_lambda" {
+  function_name    = "example-lambda"
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  role = "<IAM-role-ARN>"  # TODO
+
+  environment {
+    variables = {
+      EXAMPLE_VAR = "example-value"
     }
   }
 }
- 
-provider "aws" {
-  region = "us-west-1"
+
+resource "aws_api_gateway_rest_api" "example_api" {
+  name        = "example-api"
+  description = "API Gateway Example"
+  body        = file("oas.json")
 }
 
-resource "aws_iam_role" "test" {
-  name = "test-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
- 
-resource "aws_iam_policy" "test" {
-  name        = "test-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = ["arn:aws:logs:*:*:*"]
-    },{
-      Effect = "Allow"
-      Action = [
-        "ec2:CreateNetworkInterface",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DeleteNetworkInterface"
-      ]
-      Resource = ["*"]
-    }]
-  })
-}
- 
-resource "aws_iam_role_policy_attachment" "test" {
-  policy_arn = aws_iam_policy.test.arn
-  role = aws_iam_role.test.name
+resource "aws_api_gateway_integration" "example_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example_api.id
+  resource_id             = aws_api_gateway_rest_api.example_api.root_resource_id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.example_lambda.invoke_arn
 }
 
-resource "aws_lambda_function" "test" {
-  function_name    = "test-lambda"
-  filename         = "lambda_payload.zip"
-  source_code_hash = filebase64sha256("lambda_payload.zip")
-  handler          = "index.handler"
-  role             = aws_iam_role.test.arn
-  runtime          = "nodejs16.x"
-
+resource "aws_api_gateway_deployment" "example_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  stage_name  = "prod"
 }
 
-#################  API GATEWAY ###################
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "serverless_lambda_gw"
-  protocol_type = "HTTP"
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.example_deployment.invoke_url
 }
-
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  name        = "serverless_lambda_stage"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
-}
-
-resource "aws_apigatewayv2_integration" "funcion_lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  integration_uri    = aws_lambda_function.test.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "process" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  route_key = "Get /process"
-  target    = "integrations/${aws_apigatewayv2_integration.funcion_lambda.id}"
-}
-
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-  retention_in_days = 30
-}
-
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.test.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-}
-
